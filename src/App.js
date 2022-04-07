430872c139e2a6969880dc4e16a80d4ab782ae84
jin.xiao <jin.xiao@shopee.com>
2022-04-07 21:17:27 +0800

    feat: freeze

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Freeze } from "react-freeze";
+import { useState } from "react";
 
 function App() {
+  const [state, setState] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          setState((state) => !state);
+        }}
+      >
+        {String(state)}
+      </button>
+      <Freeze freeze={state}>
+        <div>hello world</div>
+        input: <input type="text" />
+      </Freeze>
+      <Freeze freeze={!state}>
+        <div>foo bar</div>
+        input: <input type="text" />
+      </Freeze>
     </div>
   );
 }
